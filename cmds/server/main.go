c4a283617494ec288897751b309a10262e868534
Shaun Dowling <shaun.dowling@gmail.com>
2018-01-24 17:09:47 +0000

    init: server that returns a black png with the given dimensions

---
+++ cmds/server/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+var (
+	addr = flag.String("addr", ":9000", "listen address")
+)
+
+func main() {
+	flag.Parse()
+
+	log.Println("starting image placeholder server")
+
+	http.HandleFunc("/", placeholderHandler)
+
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func placeholderHandler(rw http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/favicon.ico" {
+		return
+	}
+
+	parts := strings.Split(r.URL.Path, "/")
+	width := 600
+	height := 400
+
+	if len(parts) >= 2 {
+		dimensions := strings.Split(parts[1], "x")
+		le := len(dimensions)
+
+		if le == 2 {
+			w, err := strconv.ParseInt(dimensions[0], 10, 32)
+			if err != nil {
+				log.Println("error converting width to integer")
+				rw.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			h, err := strconv.ParseInt(dimensions[1], 10, 32)
+			if err != nil {
+				log.Println("error converting height to integer")
+				rw.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			width = int(w)
+			height = int(h)
+		}
+
+		if le == 1 {
+			w, err := strconv.ParseInt(dimensions[0], 10, 32)
+			if err != nil {
+				log.Println("error converting width to integer")
+				rw.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			width = int(w)
+			height = int(w)
+		}
+	}
+
+	phOpts := &PlaceholderOptions{
+		Width:  width,
+		Height: height,
+	}
+	ph, err := GeneratePlaceholder(phOpts)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "image/png")
+	rw.Header().Set("Content-Length", strconv.Itoa(len(*ph)))
+	rw.Write(*ph)
+}
+
+type PlaceholderOptions struct {
+	Width  int
+	Height int
+}
+
+// GeneratePlaceholder creates and encodes a png image with the given placeholder options
+func GeneratePlaceholder(opts *PlaceholderOptions) (*[]byte, error) {
+	// Create the blank graphic
+	m := image.NewRGBA(image.Rect(0, 0, opts.Width, opts.Height))
+	color := color.RGBA{0, 0, 0, 255}
+	draw.Draw(m, m.Bounds(), &image.Uniform{color}, image.ZP, draw.Src)
+
+	// Encode as a png
+	buf := new(bytes.Buffer)
+	if err := png.Encode(buf, m); err != nil {
+		return nil, fmt.Errorf("error encoding image")
+	}
+	byt := buf.Bytes()
+	return &byt, nil
+}
